19864530fc805180fe38e2ff26a253c731b0bd13
Nikita Boiko <nikbo100101@gmail.com>
2022-11-15 13:22:41 +0200

    update

--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 
 public class Menu {
@@ -9,5 +8,21 @@ public class Menu {
         this.meals = new ArrayList<>();
     }
 
-    // implement the required methods here
+    public void addMeal(String meal) {
+        if (meals.contains(meal)) {
+            System.out.println("Meal is already on the list!");
+        } else {
+            this.meals.add(meal);
+        }
+    }
+
+    public void clearMenu() {
+        this.meals.clear();
+    }
+
+    public void printMeals() {
+        for (String meal : this.meals) {
+            System.out.println(meal);
+        }
+    }
 }
