b555da2383f872c956105efc33c3aae098000665
nikiniki79 <75273923+nikiniki79@users.noreply.github.com>
2022-12-26 16:53:03 +0200

    Update RecipeSearch.java

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -1,6 +1,3 @@
-
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RecipeSearch {
@@ -8,6 +5,9 @@ public class RecipeSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("File to read: ");
+        
+        UI ui = new UI(scanner);
+        ui.start();
     }
-
 }
